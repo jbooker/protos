b84077e103343d3b516c1ac040952f1ca2acbc2e
Joe Booker <joey.booker@gmail.com>
2025-07-09 08:59:34 -0600

    Prepare for v1.0.0 release - updated TypeScript package

---
+++ packages/typescript/src/index.ts
@@ -0,0 +1,9 @@
+// Export all generated protobuf models
+export * from './chart';
+export * from './company_info';
+export * from './option_chain';
+export * from './quote';
+export * from './wheelstrategy';
+
+// Export Google protobuf dependencies if needed
+export * from './google/protobuf/timestamp';
